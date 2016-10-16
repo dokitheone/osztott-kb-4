496cf861eaf032dc9169441ecd0e86db6009d313
Viktor <kviktor92@gmail.com>
2016-10-16 16:50:32 +0200

    client fixed

--- Client.java
+++ Client.java
@@ -5,16 +5,15 @@ public class Client {
     
     public static void main(String[] args) throws RemoteException, NotBoundException {
         Registry reg = LocateRegistry.getRegistry(12345);
-        /*String[] names = reg.list();
-        for(String n : names) {
-            KodInterface ki = (KodInterface) reg.lookup(n);
-            System.out.println(n + " " + ki.getCode(n.charAt(0)));
-        }*/ 
-
         System.out.println(args[0]);
-        for(int i = 0; i < args[0].length(); i++) {
-            KodInterface ki = (KodInterface) reg.lookup(""+args[0].charAt(i));
-            System.out.println(args[0].charAt(i) + " " + ki.getCode(args[0].charAt(i)));
+        String[] names = reg.list();
+        for (int i = 0; i < args[0].length(); i++) {
+            for (int j = 0; j < names.length; j++) {
+                KodInterface ki = (KodInterface) reg.lookup(names[j]);
+                if (args[0].charAt(i) == names[j].charAt(0)) {
+                    System.out.println(args[0].charAt(i) + " " + ki.getCode(args[0].charAt(i)));
+                }
+            }
         }
     }
 }
